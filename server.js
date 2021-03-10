6630d4c84c86eaea55406f4193a4360af0f9686b
dat.ngthanh1705 <dat.ngthanh1705@gmail.com>
2021-03-10 12:09:26 +0700

    update dist file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/LungDiseaseDiagnosticSupport'));
+app.use(express.static(__dirname + '/dist/lung-disease-diagnostic-support-website'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/LungDiseaseDiagnosticSupport/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/lung-disease-diagnostic-support-website/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
